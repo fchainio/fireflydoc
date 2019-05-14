b3896b551802de6867003c6d93a03eb6e4e9eb81
imloama <imloama@gmail.com>
2019-05-14 10:32:36 +0800

    feat: rm theme config and use components

---
+++ src/.vuepress/components/locales.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  beforeMount(){
+    const lang = (window.navigator.appName=='Netscape'?window.navigator.language:window.navigator.userLanguage).toLowerCase();
+    if('zh' === lang || 'zh-cn' === lang){
+     // this.$router.push(`/`)
+    }else{
+      this.$router.push(`/en/`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
